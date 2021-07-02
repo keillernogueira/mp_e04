764987a0b664ea471aa72211203f5fd5744542c1
João Lucas <51100086+JNDJ0@users.noreply.github.com>
2021-07-02 08:02:15 -0300

    #3 - Atualização da create_dataset
    Junção das duas funções em apenas uma: por enquanto, ela ainda funciona com o LFW.

--- pessoas/manipulate_dataset.py
+++ pessoas/manipulate_dataset.py
@@ -6,17 +6,19 @@ import torch
 
 from imageio import imread, imwrite
 from dataloaders.image_dataloader import ImageDataLoader
+#from dataloaders.generic_dataloader import GenericDataLoader
 from dataloaders.LFW_dataloader import LFW
 from main import extract_features_from_image
 from argparse import ArgumentParser
 from dataset_processor import extract_features
 
-def create_dataset(save_dir, feature_file, model_name="mobilefacenet", preprocessing_method="sphereface",
-                    crop_size=(96,112), image_path=None, dataset=None, dataset_path=None):
+def manipulate_dataset(save_dir, feature_file, model_name="mobilefacenet", preprocessing_method="sphereface",
+                    crop_size=(96,112), image_path=None, img_ID=None, dataset=None, dataset_path=None):
     """
-    Creating a new features dataset.
+    Creating a new features dataset or updating an existing one.
 
     :param image_path: Path to the image that will have its features extracted.
+    :param img_ID: ID of the image.
     :param dataset: Name of the dataset that will have its features extracted.
     :param dataset_path: Path to the dataset.
     :param save_dir: Path to the dir used to save the feature file.
@@ -26,10 +28,12 @@ def create_dataset(save_dir, feature_file, model_name="mobilefacenet", preproces
     :param crop_size: Size of the crop based on the model used.
     """
     feature_file = save_dir + feature_file
-    features = None
     image_dataloader = None 
     dataset_dataloader = None
+    gpu = False
+    operation = "extract_features"
 
+    # loading dataset or image
     if dataset is not None:
         assert dataset_path is not None
         dataset = LFW(dataset, dataset_path, "jpg", preprocessing_method, crop_size)
@@ -37,58 +41,21 @@ def create_dataset(save_dir, feature_file, model_name="mobilefacenet", preproces
                                              num_workers=2, drop_last=False)
     else:
         assert image_path is not None
-        dataset = ImageDataLoader(image_path, preprocessing_method, crop_size)
-        image_dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, 
-                        num_workers=2, drop_last=False)
-    
-    # extract the features
+        dataset = ImageDataLoader(image_path, preprocessing_method, crop_size, operation == 'extract_features')
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, 
+                    num_workers=2, drop_last=False)
+  
+    # load current features
+    features = None
+    feature_file = save_dir + feature_file
+    if feature_file is not None and os.path.isfile(feature_file):
+        features = scipy.io.loadmat(feature_file)
+
+    # extracting features
     if image_dataloader is not None:
         feature = extract_features_from_image(model_name, image_dataloader, image_path, gpu=False)
     else:
         feature = extract_features(model_name, dataset_dataloader, gpu=False)
-    if feature is not None:
-        if features is None:
-            # if there is not features, use the recently extracted one as the current features
-            features = feature
-        else:
-            # otherwise, concatenate the existing features with the recently extracted ones
-            features['feature'] = np.concatenate((features['feature'], feature['feature']), 0)
-            features['name'] = np.concatenate((features['name'], feature['name']), 0)
-            features['image'] = np.concatenate((features['image'], feature['image']), 0)
-            features['bbs'] = np.concatenate((features['bbs'], feature['bbs']), 0)
-        # save the current version of the features
-        scipy.io.savemat(feature_file, features)
-        
-def update_dataset(image_path, save_dir, img_ID, feature_file, model_name="mobilefacenet", 
-                    preprocessing_method="sphereface", crop_size=(96,112)):
-    '''
-    Extracting features from image and saving it.
-    :param image_path: Path to the analysed image.
-    :param img_ID: Name associated to the image.
-    :param feature_file: Path to the file that contains extracted features from other images.
-    :param model_name: String with the name of the model used.
-    :param preprocessing_method: String with the name of the preprocessing method used.
-    :param crop_size: Size of the crop based on the model used.
-    '''
-    assert img_ID is not None
-    assert image_path is not None
-    gpu = False
-    operation = "extract_features"
-
-    # img = Image.open(open(image_path, 'rb'))
-    # img.show()
-    dataset = ImageDataLoader(image_path, preprocessing_method, crop_size, operation == 'extract_features')
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, 
-                num_workers=2, drop_last=False)
-
-    features = None
-    feature_file = save_dir + feature_file
-    # load current features
-    if feature_file is not None and os.path.isfile(feature_file):
-        features = scipy.io.loadmat(feature_file)
-
-    # extract the features
-    feature = extract_features_from_image(model_name, dataloader, image_path, gpu)
     if feature is not None:
         if features is None:
             # if there is not features, use the recently extracted one as the current features
@@ -103,54 +70,6 @@ def update_dataset(image_path, save_dir, img_ID, feature_file, model_name="mobil
         scipy.io.savemat(feature_file, features)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='main')
-    parser.add_argument("--operation", type=str, required=True, 
-                        help='Operation that will manipulate the dataset [create_dataset | update_dataset]')
-    parser.add_argument('--feature_file', type=str, required=True, help='Feature file path')
-    parser.add_argument('--save_dir', type=str, required=True,
-                        help='Path to to save outcomes of the algorithm')
+    manipulate_dataset("features/", "featurestestando123.mat", dataset="datasets/LFW", 
+                    dataset_path="images")
     
-    #image processing options
-    parser.add_argument('--image_path', type=str, default=None, help='Image path or link')
-    parser.add_argument('--image_id', type=str, default=None, 
-                        help='Image ID, **REQUIRED** if flag --operation is set as update_dataset.') 
-    
-    #dataset options
-    parser.add_argument('--dataset', type=str, default=None, help='Dataset name')
-    parser.add_argument('--dataset_path', type=str, default=None,
-                        help='Specific dataset folder. **REQUIRED** if flag --dataset is used.')
-
-    #processing options
-    parser.add_argument('--model_name', type=str, required=False, default="mobilefacenet",
-                        help='Name of the method.')
-    parser.add_argument('--preprocessing_method', type=str, required=False, default="sphereface",
-                        help='Pre-processing method')
-    parser.add_argument('--crop_size', type=int, nargs="+", required=False, default=(96, 112),
-                        help='Crop size')
-    
-    args = parser.parse_args()
-    args.crop_size = tuple(args.crop_size)
-    print(args)
-
-    #processing whole dataset
-    if args.dataset is not None:
-        if args.operation == "create_dataset":
-            create_dataset(args.save_dir, args.feature_file, args.model_name, args.preprocessing_method, 
-                            args.crop_size, dataset=args.dataset, dataset_path=args.dataset_path)
-        elif args.operation == "update_dataset":
-            raise NotImplementedError("Datasets features files cannot be updated passing datasets as argument")
-        else:
-            raise NotImplementedError("Operation " + args.operation + " not implemented")
-    
-    #processing single image
-    if args.image_path is not None:
-        if args.operation == "create_dataset":
-            create_dataset(args.save_dir, args.feature_file, args.model_name,
-                            args.preprocessing_method, args.crop_size, image_path=args.image_path)
-        elif args.operation == "update_dataset":
-            update_dataset(args.image_path, args.save_dir, args.image_id, args.feature_file, 
-                            args.model_name, args.preprocessing_method, args.crop_size)
-        else:
-            raise NotImplementedError("Operation " + args.operation + " not implemented")
-    else:
-        raise NotImplementedError("Dataset OR Image Path flags must be set. Both flags are None.")
