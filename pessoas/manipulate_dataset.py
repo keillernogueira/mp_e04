fb168beff496af251fae701d59a15c1d4e9dd349
João Lucas <51100086+JNDJ0@users.noreply.github.com>
2021-07-02 15:29:22 -0300

    #3 - Atualização manipulate_dataset

--- pessoas/manipulate_dataset.py
+++ pessoas/manipulate_dataset.py
@@ -6,22 +6,20 @@ import torch
 
 from imageio import imread, imwrite
 from dataloaders.image_dataloader import ImageDataLoader
-#from dataloaders.generic_dataloader import GenericDataLoader
+from dataloaders.generic_dataloader import GenericDataLoader
 from dataloaders.LFW_dataloader import LFW
 from main import extract_features_from_image
 from argparse import ArgumentParser
 from dataset_processor import extract_features
 
-def manipulate_dataset(save_dir, feature_file, model_name="mobilefacenet", preprocessing_method="sphereface",
-                    crop_size=(96,112), image_path=None, img_ID=None, dataset=None, dataset_path=None):
+def manipulate_dataset(feature_file,  image_path=None, img_ID=None, dataset_path=None, 
+                    model_name="mobilefacenet", preprocessing_method="sphereface", crop_size=(96,112), gpu=False):
     """
     Creating a new features dataset or updating an existing one.
 
     :param image_path: Path to the image that will have its features extracted.
     :param img_ID: ID of the image.
-    :param dataset: Name of the dataset that will have its features extracted.
     :param dataset_path: Path to the dataset.
-    :param save_dir: Path to the dir used to save the feature file.
     :param feature_file: String with the name of the feature file that will be created.
     :param model_name: String with the name of the model used.
     :param preprocessing_method: String with the name of the preprocessing method used.
@@ -29,33 +27,33 @@ def manipulate_dataset(save_dir, feature_file, model_name="mobilefacenet", prepr
     """
     image_dataloader = None 
     dataset_dataloader = None
-    gpu = False
     operation = "extract_features"
 
     # loading dataset or image
-    if dataset is not None:
-        assert dataset_path is not None
-        dataset = LFW(dataset, dataset_path, "jpg", preprocessing_method, crop_size)
-        dataset_dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=False,
-                                             num_workers=2, drop_last=False)
-    else:
-        assert image_path is not None
+    if dataset_path is not None:
+        dataset = GenericDataLoader(dataset_path, preprocessing_method='sphereface', crop_size=(96, 112))
+        dataset_dataloader = torch.utils.data.DataLoader(dataset, batch_size=8,
+                                                   shuffle=True, num_workers=4, drop_last=False)
+    elif image_path is not None:
         dataset = ImageDataLoader(image_path, preprocessing_method, crop_size, operation == 'extract_features')
         image_dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, 
                     num_workers=2, drop_last=False)
+    else:
+        raise NotImplementedError("Dataset OR Image Path flags must be set. Both flags are None.")
   
     # load current features
     features = None
     if feature_file is not None and os.path.isfile(feature_file):
         features = scipy.io.loadmat(feature_file)
-    else:
-        feature_file = save_dir + feature_file
 
     # extracting features
     if image_dataloader is not None:
-        feature = extract_features_from_image(model_name, image_dataloader, image_path, gpu=False)
+        # TODO: validar o cropped_image quando o features_file nao existe
+        feature = extract_features_from_image(model_name, image_dataloader, image_path, gpu=gpu)
+    elif dataset_dataloader is not None:
+        feature = extract_features(model_name, dataset_dataloader, gpu=gpu)
     else:
-        feature = extract_features(model_name, dataset_dataloader, gpu=False)
+        raise NotImplementedError("Both dataloaders are None.")
     if feature is not None:
         if features is None:
             # if there is not features, use the recently extracted one as the current features
@@ -70,5 +68,5 @@ def manipulate_dataset(save_dir, feature_file, model_name="mobilefacenet", prepr
         scipy.io.savemat(feature_file, features)
 
 if __name__ == '__main__':
-    manipulate_dataset("results/", "features/datasetnewfeaturesultramega.mat", image_path="images/test15.jpg", img_ID="JNDJ")
+    manipulate_dataset("features/featurestestando123.mat", dataset_path="datasets/LFW/images")
     
