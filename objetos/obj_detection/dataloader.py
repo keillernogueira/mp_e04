48f59856c04ffc2aad351e01a015ce338d0ab0d8
pedrohtg <pehtg13@gmail.com>
2021-10-07 19:30:35 -0300

    Dataset/loader changes #10

--- objetos/obj_detection/dataloader.py
+++ objetos/obj_detection/dataloader.py
@@ -7,10 +7,11 @@ def collate_fn(batch):
     return tuple(zip(*batch))
 
 def create_dataloader(dataset_path, img_size, batch_size, num_classes=11):
+    bs = {'train': batch_size, 'test': 1}
     image_datasets = {x: ListDataset(dataset_path, x, img_size, num_classes=num_classes) for x in ['train', 'test']}
 
     dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x],
-                        batch_size=batch_size, shuffle=True, num_workers=0, drop_last=False,
+                        batch_size=bs[x], shuffle=True, num_workers=0, drop_last=False,
                         collate_fn=collate_fn) 
                         for x in ['train', 'test']}
 
