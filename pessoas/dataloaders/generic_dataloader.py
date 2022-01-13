85ce2f3bcef90ae81afe353e726d41e8eb000255
henriquemoc <henriquemoc2@gmail.com>
2022-01-13 15:12:44 -0300

    Update generic_dataloader.py
    Suportar imagens rgba

--- pessoas/dataloaders/generic_dataloader.py
+++ pessoas/dataloaders/generic_dataloader.py
@@ -56,6 +56,10 @@ class GenericDataLoader(object):
         # if image is grayscale, transform into rgb by repeating the image 3 times
         if len(img.shape) == 2:
             img = np.stack([img] * 3, 2)
+        
+        #rgba to rgb
+        if img.shape[2]==4: 
+            img = img[:,:,:3]
 
         img, bb = self.preprocess.preprocess(img)
         img = img.squeeze()
