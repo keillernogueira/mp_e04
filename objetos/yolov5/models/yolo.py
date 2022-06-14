ca6a020b2c33f6795a145eca35100ccefc9ebc89
pedrohtg <pehtg13@gmail.com>
2022-06-14 11:56:58 -0300

    detection/id_person form integration to test

--- objetos/yolov5/models/yolo.py
+++ objetos/yolov5/models/yolo.py
@@ -15,9 +15,9 @@ logger = logging.getLogger(__name__)
 
 from .common import *
 from .experimental import *
-from ..utils.autoanchor import check_anchor_order
-from ..utils.general import make_divisible, check_file, set_logging
-from ..utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
+from objetos.yolov5.utils.autoanchor import check_anchor_order
+from objetos.yolov5.utils.general import make_divisible, check_file, set_logging
+from objetos.yolov5.utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
     select_device, copy_attr
 
 try:
