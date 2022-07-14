becedcf532754998d365a70bcece80fb45ad4371
VictorGabrielM <victorgabrielmoura92@gmail.com>
2022-07-14 17:19:29 -0300

    UpdateDb detail

--- frontend/mp/e04/views.py
+++ frontend/mp/e04/views.py
@@ -618,10 +618,26 @@ def update_train_detail(request, operation_id):
     operation = Operation.objects.get(id=operation_id)
     database_images = ImageDB.objects.filter(operation__id=operation_id)[0].database
 
+<<<<<<< HEAD
+
+    paginator = Paginator(image_list,20)
+    page = request.GET.get('page')
+    try:
+        response = paginator.page(page)
+    except PageNotAnInteger:
+        response = paginator.page(1)
+    except EmptyPage:
+        response = paginator.page(paginator.num_pages)
+
+    context = {'response':response,'operation':operation, 'database':database_images,'qtt_images':image_list.count}
+
+    return render(request,'e04/updateDB_detail.html',context)
+=======
     context = {'image_list': image_list, 'operation': operation, 'database': database_images}
 
     return render(request, 'e04/update_train_detail.html', context)
 
+>>>>>>> b69d99851567384ee9dbc4a534ead8b5cd8855c5
 
 @login_required
 def detailed_result(request, operation_id):
